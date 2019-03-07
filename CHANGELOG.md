b2761f6ee2c8f48930f88cef6b0b9b67cd6b900e
Michail Shipov <mshipov@yandex.ru>
2019-03-07 21:42:01 +0300

    fix: add npm "builtin" config support
    PR #1
    
    * fix links to npm github repo
    
    * add builtin npm config (if present) to config chain
    
    * bump engine version to >=8.9 and add changelog
    
    * add check for require.resolve.paths existance

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Unpublished
+ * Fix links to `npm` repo
+## BREAKING CHANGE
+ * Add npm builtin config to config chain. This requires minimum `8.9` version of `node`
--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const path = require('path');
 const Conf = require('./lib/conf');
 const _defaults = require('./lib/defaults');
 
-// https://github.com/npm/npm/blob/latest/lib/config/core.js#L101-L200
+// https://github.com/npm/cli/blob/latest/lib/config/core.js#L101-L200
 module.exports = (opts, types, defaults) => {
 	const conf = new Conf(Object.assign({}, _defaults.defaults, defaults), types);
 
 	conf.add(Object.assign({}, opts), 'cli');
+
+	if (require.resolve.paths) {
+		const paths = require.resolve.paths('npm');
+		// assume that last path in resolve paths is builtin modules directory
+		const npmPath = require.resolve('npm', {paths: paths.slice(-1)});
+		if (npmPath) {
+			/**
+			 *  according to https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/npm.js#L258
+			 *  and https://github.com/npm/cli/blob/86f5bdb91f7a5971953a5171d32d6eeda6a2e972/lib/config/core.js#L92
+			 */
+			conf.addFile(path.resolve(path.dirname(npmPath), '..', 'npmrc'), 'builtin');
+		}
+	}
+
 	conf.addEnv();
 	conf.loadPrefix();
 
